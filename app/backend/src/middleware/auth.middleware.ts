c233a5d45a3453ba8fbcabe309a92903845d16bf
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-04-05 16:16:24 +0530

    Fix auth middleware

--- app/backend/src/middleware/auth.middleware.ts
+++ app/backend/src/middleware/auth.middleware.ts
@@ -8,7 +8,12 @@ export async function authMiddleware(
     next: NextFunction
 ) {
     try {
-        const token = req.headers.authorization?.split(" ")[1];
+
+        if(!req.headers.authorization || !req.headers.authorization.startsWith('Bearer')) {
+            throw new ApiError(401, "Unauthotized access")
+        }
+
+        const token = req.headers.authorization.split(" ")[1];
 
         if (!token || !process.env.JWT_SECRET) {
             throw new ApiError(401, "Invalid secret")
